4410619a964e851585566ab723760c028d338019
oleg.strelnikov <oleg.strelnikov@gmail.com>
2014-11-21 03:11:53 +0400

    Dictionary implementation

--- dictionary.hpp
+++ dictionary.hpp
@@ -8,5 +8,24 @@
 #ifndef DICTIONARY_HPP_
 #define DICTIONARY_HPP_
 
+#include <map>
+
+namespace reet {
+
+template <typename Key, typename Data, typename Map = std::map<Key, Data> > class Dictionary {
+	typedef Key key_type;
+	typedef Data data_type;
+	typedef Map map_type;
+public:
+	Dictionary(map_type const& dictionary, data_type const& notFound) : dictionary_(dictionary), notFound_(notFound) {}
+	data_type const& get(key_type const& key) const {
+		return (dictionary_.find(key) != dictionary_.end() ? dictionary_.at(key) : notFound_);
+	}
+private:
+	const map_type dictionary_;
+	const data_type notFound_;
+}; //class Dictionary
+
+} //namespace reet
 
 #endif /* DICTIONARY_HPP_ */
